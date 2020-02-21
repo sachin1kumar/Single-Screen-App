238034af7bee50d46707ce4bc2e4934e35f4d82e
Sachin <sachinkmr375@gmail.com>
2020-02-21 14:40:34 +0530

    added toolbar with menu inflator.

--- app/src/main/java/com/manager/singlescreenapp/MainActivity.kt
+++ app/src/main/java/com/manager/singlescreenapp/MainActivity.kt
@@ -2,11 +2,32 @@ package com.manager.singlescreenapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        
+        return true
+    }
+
+
 }
